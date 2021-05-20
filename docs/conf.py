66f9dc99dc502e0edd358ca32f360c033b9d0345
Christian Meyer <christian.meyer@tum.de>
2021-05-20 11:18:27 +0200

    Added .readthedocs.yaml and updated sphinx doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/ammorph/'))
+# sys.path.insert(0, os.path.abspath('../src/ammorph/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,6 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
